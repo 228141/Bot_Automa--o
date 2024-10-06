ae52cc221bb77b6cd4c893528a2ccfc1f855f537
228141 <jailton.sp@hotmail.com>
2024-10-06 09:18:00 -0300

    Estudando python

---
+++ 001.py
@@ -0,0 +1,46 @@
+'''
+No python a variaveis são sempre objetos.
+O simbolo "=" quer dizer recebe. 
+
+"print" significa escreva na tela
+"input" significa Leia
+'''
+
+nome = input ('Qual é o seu nome?')
+idade = input ('Qual a sua idade?')
+peso = input ('Qual o seu peso?')       
+print (nome, idade, peso)
+
+'''
+--Desafio 01
+
+Crie um script que leia o nome de uma pessoa e mostre uma mensagem de boas vindas de acordo com o valor digitado.
+
+nome = input ('Qual é o seu nome?')
+print ('Olá,'),print(nome),print('seja bem vindo')
+
+
+-- Desafio 02
+
+Crie um script Python que leia o dia o mês e o ano de nascimento de uma pessoa e mostre uma mensagem com a data formatada. 
+
+dia = input ('Que dia você nasceu?')
+mês = input ('Que mês você nasceu?')
+ano = input ('Que ano você nasceu?')
+print ('Você nasceu', dia, mês, ano)
+
+
+--Desafio 03
+
+Crie um script que leia dois números e tente mostrar a soma entre eles.
+# Solicita ao usuário para inserir o primeiro número
+numero1 = float(input("Digite o primeiro número: "))
+# Solicita ao usuário para inserir o segundo número
+numero2 = float(input("Digite o segundo número: "))
+# Calcula a soma dos dois números
+soma = numero1 + numero2
+# Exibe o resultado
+print(f"A soma de {numero1} e {numero2} é {soma}.")
+
+
+'''
